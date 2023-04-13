b73c11015a6599fe5f2c40cf02d0489e6556fb5e
Pekka Enberg <penberg@iki.fi>
2023-04-13 10:13:19 +0300

    Reorder code

--- core/mvcc/database/src/database.rs
+++ core/mvcc/database/src/database.rs
@@ -87,144 +87,6 @@ pub struct Database<Clock: LogicalClock> {
 
 type TxID = u64;
 
-#[derive(Debug)]
-pub struct DatabaseInner<Clock: LogicalClock> {
-    rows: RefCell<HashMap<u64, Vec<RowVersion>>>,
-    txs: RefCell<HashMap<TxID, Transaction>>,
-    tx_ids: AtomicU64,
-    clock: Clock,
-}
-
-impl<Clock: LogicalClock> DatabaseInner<Clock> {
-    fn insert(&self, tx_id: TxID, row: Row) -> Result<()> {
-        let mut txs = self.txs.borrow_mut();
-        let tx = txs
-            .get_mut(&tx_id)
-            .ok_or(DatabaseError::NoSuchTransactionID(tx_id))?;
-        assert!(tx.state == TransactionState::Active);
-        let id = row.id;
-        let row_version = RowVersion {
-            begin: TxTimestampOrID::TxID(tx.tx_id),
-            end: None,
-            row,
-        };
-        let mut rows = self.rows.borrow_mut();
-        rows.entry(id).or_insert_with(Vec::new).push(row_version);
-        tx.insert_to_write_set(id);
-        Ok(())
-    }
-
-    fn delete(&self, tx_id: TxID, id: u64) -> Result<bool> {
-        let mut rows = self.rows.borrow_mut();
-        let mut txs = self.txs.borrow_mut();
-        if let Some(row_versions) = rows.get_mut(&id) {
-            for rv in row_versions.iter_mut().rev() {
-                let tx = txs
-                    .get(&tx_id)
-                    .ok_or(DatabaseError::NoSuchTransactionID(tx_id))?;
-                assert!(tx.state == TransactionState::Active);
-                if is_write_write_conflict(&txs, tx, rv) {
-                    drop(txs);
-                    drop(rows);
-                    self.rollback_tx(tx_id);
-                    return Err(DatabaseError::WriteWriteConflict);
-                }
-                if is_version_visible(&txs, tx, rv) {
-                    rv.end = Some(TxTimestampOrID::TxID(tx.tx_id));
-                    let tx = txs
-                        .get_mut(&tx_id)
-                        .ok_or(DatabaseError::NoSuchTransactionID(tx_id))?;
-                    tx.insert_to_write_set(id);
-                    return Ok(true);
-                }
-            }
-        }
-        Ok(false)
-    }
-
-    fn read(&self, tx_id: TxID, id: u64) -> Result<Option<Row>> {
-        let txs = self.txs.borrow_mut();
-        let tx = txs.get(&tx_id).unwrap();
-        assert!(tx.state == TransactionState::Active);
-        let rows = self.rows.borrow();
-        if let Some(row_versions) = rows.get(&id) {
-            for rv in row_versions.iter().rev() {
-                if is_version_visible(&txs, tx, rv) {
-                    tx.insert_to_read_set(id);
-                    return Ok(Some(rv.row.clone()));
-                }
-            }
-        }
-        Ok(None)
-    }
-
-    fn begin_tx(&mut self) -> TxID {
-        let tx_id = self.get_tx_id();
-        let begin_ts = self.get_timestamp();
-        let tx = Transaction::new(tx_id, begin_ts);
-        let mut txs = self.txs.borrow_mut();
-        txs.insert(tx_id, tx);
-        tx_id
-    }
-
-    fn commit_tx(&mut self, tx_id: TxID) -> Result<()> {
-        let end_ts = self.get_timestamp();
-        let mut txs = self.txs.borrow_mut();
-        let mut tx = txs.get_mut(&tx_id).unwrap();
-        match tx.state {
-            TransactionState::Terminated => return Err(DatabaseError::TxTerminated),
-            _ => {
-                assert!(tx.state == TransactionState::Active);
-            }
-        }
-        let mut rows = self.rows.borrow_mut();
-        tx.state = TransactionState::Preparing;
-        for id in &tx.write_set {
-            if let Some(row_versions) = rows.get_mut(id) {
-                for row_version in row_versions.iter_mut() {
-                    if let TxTimestampOrID::TxID(id) = row_version.begin {
-                        if id == tx_id {
-                            row_version.begin = TxTimestampOrID::Timestamp(tx.begin_ts);
-                        }
-                    }
-                    if let Some(TxTimestampOrID::TxID(id)) = row_version.end {
-                        if id == tx_id {
-                            row_version.end = Some(TxTimestampOrID::Timestamp(end_ts));
-                        }
-                    }
-                }
-            }
-        }
-        tx.state = TransactionState::Committed;
-        Ok(())
-    }
-
-    fn rollback_tx(&self, tx_id: TxID) {
-        let mut txs = self.txs.borrow_mut();
-        let mut tx = txs.get_mut(&tx_id).unwrap();
-        assert!(tx.state == TransactionState::Active);
-        tx.state = TransactionState::Aborted;
-        let mut rows = self.rows.borrow_mut();
-        for id in &tx.write_set {
-            if let Some(row_versions) = rows.get_mut(id) {
-                row_versions.retain(|rv| rv.begin != TxTimestampOrID::TxID(tx_id));
-                if row_versions.is_empty() {
-                    rows.remove(id);
-                }
-            }
-        }
-        tx.state = TransactionState::Terminated;
-    }
-
-    fn get_tx_id(&mut self) -> u64 {
-        self.tx_ids.fetch_add(1, Ordering::SeqCst)
-    }
-
-    fn get_timestamp(&mut self) -> u64 {
-        self.clock.get_timestamp()
-    }
-}
-
 impl<Clock: LogicalClock> Database<Clock> {
     /// Creates a new database.
     pub fn new(clock: Clock) -> Self {
@@ -374,6 +236,144 @@ fn is_write_write_conflict(
         }
         Some(TxTimestampOrID::Timestamp(_)) => false,
         None => false,
+    }
+}
+
+#[derive(Debug)]
+pub struct DatabaseInner<Clock: LogicalClock> {
+    rows: RefCell<HashMap<u64, Vec<RowVersion>>>,
+    txs: RefCell<HashMap<TxID, Transaction>>,
+    tx_ids: AtomicU64,
+    clock: Clock,
+}
+
+impl<Clock: LogicalClock> DatabaseInner<Clock> {
+    fn insert(&self, tx_id: TxID, row: Row) -> Result<()> {
+        let mut txs = self.txs.borrow_mut();
+        let tx = txs
+            .get_mut(&tx_id)
+            .ok_or(DatabaseError::NoSuchTransactionID(tx_id))?;
+        assert!(tx.state == TransactionState::Active);
+        let id = row.id;
+        let row_version = RowVersion {
+            begin: TxTimestampOrID::TxID(tx.tx_id),
+            end: None,
+            row,
+        };
+        let mut rows = self.rows.borrow_mut();
+        rows.entry(id).or_insert_with(Vec::new).push(row_version);
+        tx.insert_to_write_set(id);
+        Ok(())
+    }
+
+    fn delete(&self, tx_id: TxID, id: u64) -> Result<bool> {
+        let mut rows = self.rows.borrow_mut();
+        let mut txs = self.txs.borrow_mut();
+        if let Some(row_versions) = rows.get_mut(&id) {
+            for rv in row_versions.iter_mut().rev() {
+                let tx = txs
+                    .get(&tx_id)
+                    .ok_or(DatabaseError::NoSuchTransactionID(tx_id))?;
+                assert!(tx.state == TransactionState::Active);
+                if is_write_write_conflict(&txs, tx, rv) {
+                    drop(txs);
+                    drop(rows);
+                    self.rollback_tx(tx_id);
+                    return Err(DatabaseError::WriteWriteConflict);
+                }
+                if is_version_visible(&txs, tx, rv) {
+                    rv.end = Some(TxTimestampOrID::TxID(tx.tx_id));
+                    let tx = txs
+                        .get_mut(&tx_id)
+                        .ok_or(DatabaseError::NoSuchTransactionID(tx_id))?;
+                    tx.insert_to_write_set(id);
+                    return Ok(true);
+                }
+            }
+        }
+        Ok(false)
+    }
+
+    fn read(&self, tx_id: TxID, id: u64) -> Result<Option<Row>> {
+        let txs = self.txs.borrow_mut();
+        let tx = txs.get(&tx_id).unwrap();
+        assert!(tx.state == TransactionState::Active);
+        let rows = self.rows.borrow();
+        if let Some(row_versions) = rows.get(&id) {
+            for rv in row_versions.iter().rev() {
+                if is_version_visible(&txs, tx, rv) {
+                    tx.insert_to_read_set(id);
+                    return Ok(Some(rv.row.clone()));
+                }
+            }
+        }
+        Ok(None)
+    }
+
+    fn begin_tx(&mut self) -> TxID {
+        let tx_id = self.get_tx_id();
+        let begin_ts = self.get_timestamp();
+        let tx = Transaction::new(tx_id, begin_ts);
+        let mut txs = self.txs.borrow_mut();
+        txs.insert(tx_id, tx);
+        tx_id
+    }
+
+    fn commit_tx(&mut self, tx_id: TxID) -> Result<()> {
+        let end_ts = self.get_timestamp();
+        let mut txs = self.txs.borrow_mut();
+        let mut tx = txs.get_mut(&tx_id).unwrap();
+        match tx.state {
+            TransactionState::Terminated => return Err(DatabaseError::TxTerminated),
+            _ => {
+                assert!(tx.state == TransactionState::Active);
+            }
+        }
+        let mut rows = self.rows.borrow_mut();
+        tx.state = TransactionState::Preparing;
+        for id in &tx.write_set {
+            if let Some(row_versions) = rows.get_mut(id) {
+                for row_version in row_versions.iter_mut() {
+                    if let TxTimestampOrID::TxID(id) = row_version.begin {
+                        if id == tx_id {
+                            row_version.begin = TxTimestampOrID::Timestamp(tx.begin_ts);
+                        }
+                    }
+                    if let Some(TxTimestampOrID::TxID(id)) = row_version.end {
+                        if id == tx_id {
+                            row_version.end = Some(TxTimestampOrID::Timestamp(end_ts));
+                        }
+                    }
+                }
+            }
+        }
+        tx.state = TransactionState::Committed;
+        Ok(())
+    }
+
+    fn rollback_tx(&self, tx_id: TxID) {
+        let mut txs = self.txs.borrow_mut();
+        let mut tx = txs.get_mut(&tx_id).unwrap();
+        assert!(tx.state == TransactionState::Active);
+        tx.state = TransactionState::Aborted;
+        let mut rows = self.rows.borrow_mut();
+        for id in &tx.write_set {
+            if let Some(row_versions) = rows.get_mut(id) {
+                row_versions.retain(|rv| rv.begin != TxTimestampOrID::TxID(tx_id));
+                if row_versions.is_empty() {
+                    rows.remove(id);
+                }
+            }
+        }
+        tx.state = TransactionState::Terminated;
+    }
+
+    fn get_tx_id(&mut self) -> u64 {
+        self.tx_ids.fetch_add(1, Ordering::SeqCst)
+    }
+
+    fn get_timestamp(&mut self) -> u64 {
+        self.clock.get_timestamp()
     }
 }
 
