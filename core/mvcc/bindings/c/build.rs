5ce2bc41f9209db71d78c4437ea42f18a124149a
Pekka Enberg <penberg@iki.fi>
2023-05-09 10:47:43 +0300

    cargo fmt

--- core/mvcc/bindings/c/build.rs
+++ core/mvcc/bindings/c/build.rs
@@ -2,5 +2,7 @@ use std::path::Path;
 
 fn main() {
     let header_file = Path::new("include").join("mvcc.h");
-    cbindgen::generate(".").expect("Failed to generate C bindings").write_to_file(header_file);
+    cbindgen::generate(".")
+        .expect("Failed to generate C bindings")
+        .write_to_file(header_file);
 }
