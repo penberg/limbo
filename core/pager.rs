f92a83d0830f6be81b1a3988156b5aa4c33e068c
Pekka Enberg <penberg@iki.fi>
2023-09-02 21:12:41 +0300

    Switch to LRU page cache
    Fixes #5

--- core/pager.rs
+++ core/pager.rs
@@ -4,24 +4,23 @@ use crate::sqlite3_ondisk;
 use crate::sqlite3_ondisk::BTreePage;
 use crate::DatabaseRef;
 use crate::IO;
-use anyhow::Result;
-use std::collections::HashMap;
+use concurrent_lru::unsharded::LruCache;
 use std::sync::{Arc, Mutex};
 
 pub struct Pager {
     io: Arc<dyn IO>,
     database_ref: DatabaseRef,
-    page_cache: Arc<Mutex<HashMap<usize, Arc<BTreePage>>>>,
+    page_cache: LruCache<usize, Arc<BTreePage>>,
     buffer_pool: Arc<Mutex<BufferPool>>,
 }
 
 impl Pager {
-    pub fn open(io: Arc<dyn IO>, path: &str) -> Result<Self> {
+    pub fn open(io: Arc<dyn IO>, path: &str) -> anyhow::Result<Self> {
         let database_ref = io.open(path)?;
         let db_header = sqlite3_ondisk::read_database_header(io.clone(), database_ref)?;
         let page_size = db_header.page_size as usize;
         let buffer_pool = Arc::new(Mutex::new(buffer_pool::BufferPool::new(page_size)));
-        let page_cache = Arc::new(Mutex::new(HashMap::default()));
+        let page_cache = LruCache::new(10);
         Ok(Self {
             io,
             database_ref,
@@ -30,23 +29,18 @@ impl Pager {
         })
     }
 
-    pub fn read_page(&self, page_idx: usize) -> Result<Arc<BTreePage>> {
-        {
-            let page_cache = self.page_cache.lock().unwrap();
-            if let Some(page) = page_cache.get(&page_idx) {
-                return Ok(page.clone());
-            }
-        }
-        let mut buffer_pool = self.buffer_pool.lock().unwrap();
-        let page = sqlite3_ondisk::read_btree_page(
-            self.io.clone(),
-            self.database_ref,
-            &mut buffer_pool,
-            page_idx,
-        )?;
-        // TODO: Evict pages from the page cache when it's full.
-        let mut page_cache = self.page_cache.lock().unwrap();
-        page_cache.insert(page_idx, Arc::new(page));
-        Ok(page_cache.get(&page_idx).unwrap().clone())
+    pub fn read_page(&self, page_idx: usize) -> anyhow::Result<Arc<BTreePage>> {
+        let handle = self.page_cache.get_or_try_init(page_idx, 1, |idx| {
+            let mut buffer_pool = self.buffer_pool.lock().unwrap();
+            let page = sqlite3_ondisk::read_btree_page(
+                self.io.clone(),
+                self.database_ref,
+                &mut buffer_pool,
+                page_idx,
+            )
+            .unwrap();
+            Ok::<Arc<BTreePage>, anyhow::Error>(Arc::new(page))
+        })?;
+        Ok(handle.value().clone())
     }
 }
