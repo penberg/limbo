81cf0430dea5e8b23a7a81e8c453cb6009b0b3f3
Piotr Jastrzebski <haaawk@gmail.com>
2024-07-07 13:05:00 +0200

    Improve normalize_ident
    Signed-off-by: Piotr Jastrzebski <haaawk@gmail.com>

--- core/util.rs
+++ core/util.rs
@@ -1,6 +1,7 @@
-pub fn normalize_ident(mut ident: &str) -> String {
-    if ident.starts_with('"') && ident.ends_with('"') {
-        ident = &ident[1..ident.len() - 1];
-    }
-    ident.to_lowercase()
+pub fn normalize_ident(ident: &str) -> String {
+    (if ident.starts_with('"') && ident.ends_with('"') {
+        &ident[1..ident.len() - 1]
+    } else {
+        ident
+    }).to_lowercase()
 }
