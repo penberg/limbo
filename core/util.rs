83c7e7bd950d8cbc2f35e03de6a92ded8aa3ea51
Piotr Jastrzebski <haaawk@gmail.com>
2024-07-07 12:38:37 +0200

    Remove unneeded allocation
    Signed-off-by: Piotr Jastrzebski <haaawk@gmail.com>

--- core/util.rs
+++ core/util.rs
@@ -1,6 +1,6 @@
 pub fn normalize_ident(ident: &str) -> String {
     if ident.starts_with('"') && ident.ends_with('"') {
-        ident[1..ident.len() - 1].to_string().to_lowercase()
+        ident[1..ident.len() - 1].to_lowercase()
     } else {
         ident.to_lowercase()
     }
