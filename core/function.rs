851aea212d1f5120c8f56fc9d3c731e1799ddffe
jussisaurio <jussi.saurio@gmail.com>
2024-07-13 23:03:25 +0300

    add coalesce(), refactor/rename add_label()

--- core/function.rs
+++ core/function.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 #[derive(Debug, Clone, PartialEq)]
 pub enum AggFunc {
     Avg,
@@ -21,6 +23,34 @@ impl AggFunc {
             AggFunc::StringAgg => "string_agg",
             AggFunc::Sum => "sum",
             AggFunc::Total => "total",
+        }
+    }
+}
+
+pub enum SingleRowFunc {
+    Coalesce,
+}
+
+pub enum Func {
+    Agg(AggFunc),
+    SingleRow(SingleRowFunc),
+}
+
+impl FromStr for Func {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "avg" => Ok(Func::Agg(AggFunc::Avg)),
+            "count" => Ok(Func::Agg(AggFunc::Count)),
+            "group_concat" => Ok(Func::Agg(AggFunc::GroupConcat)),
+            "max" => Ok(Func::Agg(AggFunc::Max)),
+            "min" => Ok(Func::Agg(AggFunc::Min)),
+            "string_agg" => Ok(Func::Agg(AggFunc::StringAgg)),
+            "sum" => Ok(Func::Agg(AggFunc::Sum)),
+            "total" => Ok(Func::Agg(AggFunc::Total)),
+            "coalesce" => Ok(Func::SingleRow(SingleRowFunc::Coalesce)),
+            _ => Err(()),
         }
     }
 }
