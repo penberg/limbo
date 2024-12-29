db384c6828a21d449d1c7d984650c6730b41fad0
Lauri Virtanen <lauri.virtanen@iki.fi>
2024-12-29 19:08:34 +0200

    Simplify init of `delimiter_expr`s
    Clippy:
    https://rust-lang.github.io/rust-clippy/master/index.html#needless_late_init

--- core/translate/expr.rs
+++ core/translate/expr.rs
@@ -2265,14 +2265,10 @@ pub fn translate_aggregation(
             let delimiter_reg = program.alloc_register();
 
             let expr = &agg.args[0];
-            let delimiter_expr: ast::Expr;
-
-            match &agg.args[1] {
-                ast::Expr::Column { .. } => {
-                    delimiter_expr = agg.args[1].clone();
-                }
+            let delimiter_expr = match &agg.args[1] {
+                ast::Expr::Column { .. } => agg.args[1].clone(),
                 ast::Expr::Literal(ast::Literal::String(s)) => {
-                    delimiter_expr = ast::Expr::Literal(ast::Literal::String(s.to_string()));
+                    ast::Expr::Literal(ast::Literal::String(s.to_string()))
                 }
                 _ => crate::bail_parse_error!("Incorrect delimiter parameter"),
             };
@@ -2448,14 +2444,10 @@ pub fn translate_aggregation_groupby(
             let expr_reg = program.alloc_register();
             let delimiter_reg = program.alloc_register();
 
-            let delimiter_expr: ast::Expr;
-
-            match &agg.args[1] {
-                ast::Expr::Column { .. } => {
-                    delimiter_expr = agg.args[1].clone();
-                }
+            let delimiter_expr = match &agg.args[1] {
+                ast::Expr::Column { .. } => agg.args[1].clone(),
                 ast::Expr::Literal(ast::Literal::String(s)) => {
-                    delimiter_expr = ast::Expr::Literal(ast::Literal::String(s.to_string()));
+                    ast::Expr::Literal(ast::Literal::String(s.to_string()))
                 }
                 _ => crate::bail_parse_error!("Incorrect delimiter parameter"),
             };
