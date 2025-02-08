9aedbf2d45c1826470380bda07b334f90689043a
Jorge López <jorgekq@hotmail.es>
2025-02-08 21:20:51 +0100

    core: make storage::pager::PageRef  public, because it is needed to implement trait Wal

--- core/lib.rs
+++ core/lib.rs
@@ -42,6 +42,7 @@ use storage::btree::btree_init_page;
 use storage::database::FileStorage;
 use storage::page_cache::DumbLruPageCache;
 use storage::pager::allocate_page;
+pub use storage::pager::PageRef;
 use storage::sqlite3_ondisk::{DatabaseHeader, DATABASE_HEADER_SIZE};
 pub use storage::wal::WalFile;
 pub use storage::wal::WalFileShared;
