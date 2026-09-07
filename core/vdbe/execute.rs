tursodatabase/turso

core/vdbe: put a built record into its register without the drop glue
MakeRecord, RowData and SorterData take the spent buffer out of their
destination register, which leaves a NULL there, and then assign the
new record to it. The assignment drops that NULL through Register's
drop glue, an out-of-line call of about 27 instructions that has
nothing to free.

Put the record in through a method that forgets the NULL instead, with
a debug assertion that it is one.

Measured with callgrind, instructions:
  SELECT value % 100, count(*) FROM t GROUP BY value % 100 (3 iterations, 100k rows):
    1,119,553,617 -> 1,102,115,272 (-1.6%, two records per row)
  SELECT * FROM t ORDER BY name DESC LIMIT 10: 1,061,427,316 -> 1,052,700,201 (-0.8%)
  SELECT * FROM t (3 iterations, no records):  404,917,865 -> 407,767,451 (+0.7%)
  SELECT 1 FROM t (5 iterations):              201,259,924 -> 204,297,280 (+1.5%)
The two scans build no record; their deltas are the inlining of
Program::step, which moves between the same two states with every
build (204.3M or 201.3M for the SELECT 1 scan).

Co-Authored-By: Claude Fable 5.1 <noreply@anthropic.com>
Claude-Session: https://claude.ai/code/session_01Feji9LAGhf6W4U2BBE6aWs

core/vdbe/execute.rs
@@ -3268,7 +3268,7 @@ pub fn op_make_record(
     let buf = state.registers[dest_reg].take_buf();
     let regs = &state.registers[start_reg..start_reg + count];
     let record = ImmutableRecord::build_from_registers(regs, buf)?;
-    state.registers[dest_reg] = Register::Record(record);
+    state.registers[dest_reg].put_record(record);
     state.pc += 1;
     Ok(InsnFunctionStepResult::Step)
 }
@@ -5954,10 +5954,7 @@ pub fn op_row_data(
 
         ImmutableRecord::copy_payload(record.get_payload(), buf)?
     };
-
-    let reg = &mut state.registers[*dest];
-    *reg = Register::Record(record);
-
+    state.registers[*dest].put_record(record);
     state.pc += 1;
     Ok(InsnFunctionStepResult::Step)
 }
@@ -9159,7 +9156,7 @@ pub fn op_sorter_data(
             .take_current(buf)
             .expect("sorter record checked above")
     };
-    state.registers[*dest_reg] = Register::Record(record);
+    state.registers[*dest_reg].put_record(record);
     state.pc += 1;
     Ok(InsnFunctionStepResult::Step)
 }

penberg/limbo

core/vdbe/execute.rs
@@ -3268,7 +3268,7 @@ pub fn op_make_record(
     let buf = state.registers[dest_reg].take_buf();
     let regs = &state.registers[start_reg..start_reg + count];
     let record = ImmutableRecord::build_from_registers(regs, buf)?;
-    state.registers[dest_reg] = Register::Record(record);
+    state.registers[dest_reg].put_record_into_null_register(record);
     state.pc += 1;
     Ok(InsnFunctionStepResult::Step)
 }
@@ -5954,10 +5954,7 @@ pub fn op_row_data(
 
         ImmutableRecord::copy_payload(record.get_payload(), buf)?
     };
-
-    let reg = &mut state.registers[*dest];
-    *reg = Register::Record(record);
-
+    state.registers[*dest].put_record_into_null_register(record);
     state.pc += 1;
     Ok(InsnFunctionStepResult::Step)
 }
@@ -9140,7 +9137,7 @@ pub fn op_sorter_data(
             .take_current(buf)
             .expect("sorter record checked above")
     };
-    state.registers[*dest_reg] = Register::Record(record);
+    state.registers[*dest_reg].put_record_into_null_register(record);
     state.pc += 1;
     Ok(InsnFunctionStepResult::Step)
 }